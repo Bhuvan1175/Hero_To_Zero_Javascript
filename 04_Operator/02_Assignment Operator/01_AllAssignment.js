c8a89b4a092698be95ade3e72fdfee1f2b19c4f4
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-01-02 16:00:19 +0530

    Add files via upload

---
+++ 04_Operator/02_Assignment Operator/01_AllAssignment.js
@@ -0,0 +1,25 @@
+// addition , substration , multiplication , divison , equal , modulas, exponentional
+/*
+            ==
+            +=
+            -=
+            *=
+            /=
+            %=
+            **=
+*/
+
+//CODE START
+
+var a = 7;
+var b = 5;
+var c = 25;
+console.log("Equal : " + (a == b));
+console.log("Addition : " + (a += 3));
+console.log("Substraction :" + (b -= 4));
+console.log("Multiplication :" + (c *= 10));
+console.log("Division :" + (c /= 25));
+console.log("Modulus :" + (a %= 6));
+var b = 8
+b **= 5;
+console.log("Exponential :" + b);
