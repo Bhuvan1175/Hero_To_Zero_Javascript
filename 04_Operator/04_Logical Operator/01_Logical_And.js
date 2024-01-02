5bf82d3da136e43e55852ca22bbeb99fb42dda97
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-01-02 16:39:41 +0530

    Add files via upload

---
+++ 04_Operator/04_Logical Operator/01_Logical_And.js
@@ -0,0 +1,4 @@
+var a=9;
+var b=8;
+var c=6;
+console.log("Logical And (9>8 && 9>6) : "+ (a>b && a>c));//true
---
+++ 04_Operator/04_Logical Operator/02_Logical_Or.js
@@ -0,0 +1,9 @@
+// || --> Pipe symbol
+var a = 9;
+var b = 8;
+var c = 6;
+var cond1 = a > b;
+var cond2 = c > a;
+var cond3 = c > b;
+console.log("Logical Or (9>8 || 6>9) : " + (cond1 || cond2)); //true
+console.log("Logical Or (6>9 || 6>8) : " + (cond2 || cond3)); //false
