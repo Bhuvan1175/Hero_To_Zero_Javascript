44a1e96ed09fa721604eb20f7abf1e6a94e794e5
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-01-03 18:46:35 +0530

    Add files via upload

---
+++ 06_Conditional Statement/If Else Statements/01_Marks.js
@@ -0,0 +1,8 @@
+let name=prompt("Enter Your Name :");
+let marks_Maths=parseInt(prompt("Enter You Marks Between 0 to 100 :"));
+if(marks_Maths<35){
+    console.log(name+" is FAIL");
+}
+else{
+    console.log(name+" is PASS");
+}
