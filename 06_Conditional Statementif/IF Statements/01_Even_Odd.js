6e1bf0a414631c50a264a1723f06b19d1763a011
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-01-03 18:39:23 +0530

    Add files via upload

---
+++ 06_Conditional Statementif/IF Statements/01_Even_Odd.js
@@ -0,0 +1,10 @@
+// Write A program to to check a give number is even or odd ?
+// CODE START
+
+let num1=prompt("Enter Any Number :");
+if(num1%2==0){
+    console.log(+num1+" is EVEN");
+}
+if(num1%2!=0){
+    console.log(+num1+" is ODD Number");
+}
