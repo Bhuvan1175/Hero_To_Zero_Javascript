e6b485563d379bb80f1f8873137f3c4c1735600e
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-01-05 00:36:41 +0530

    Add files via upload

---
+++ 07_Loops/02_While Loop/05_Game_Number.js
@@ -0,0 +1,6 @@
+let gameNum = 18;
+let userNum = parseInt(prompt("Enter Any Number : "));
+while (userNum !== gameNum) {
+  userNum = parseInt(prompt("You Entered Number Is Wrong , Please Renter Your Number : "));
+}
+console.log("Congratulations!! You Are The Winner Of Game Number..");
