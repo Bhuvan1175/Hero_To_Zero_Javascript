78578b71e50a5b47100fd77865218e52e04cbdc0
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-01-06 00:13:19 +0530

    Add files via upload

---
+++ 08_Strings/02_Literals/01_Interpollation.js
@@ -0,0 +1,5 @@
+// String interpolation in JavaScript refers to the process of embedding expressions or variables within a string. This can be achieved using template literals, which are strings delimited by backticks [ ` `].
+
+let str="Bhuvan Bam";
+let result=str.length;
+console.log(`Length of ${str} is ${result} `);
