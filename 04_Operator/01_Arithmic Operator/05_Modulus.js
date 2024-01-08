43c13adaefb2ebe8494a936bc8c38007442c5c04
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-01-08 20:49:57 +0530

    Update 05_Modulus.js

--- 04_Operator/01_Arithmic Operator/05_Modulus.js
+++ 04_Operator/01_Arithmic Operator/05_Modulus.js
@@ -1,6 +1,5 @@
 // Write a JavaScript code that calculates the remainder when a particular fixed number is divided by another fixed number.
 //CODE START
-
 var fixed_Num1=4443;
 var fixed_Num2=3;
 console.log("Remainder of Two Fixed Number is "+fixed_Num1%fixed_Num2);
