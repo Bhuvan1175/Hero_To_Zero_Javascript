d6bc1e1f2497ccb6aeca35a98e999e02ef9298a1
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-01-10 14:48:18 +0530

    Add files via upload

---
+++ 10_Functions/Array Methods/Filter Method/01_Voting.js
@@ -0,0 +1,5 @@
+let age = [12, 17, 48, 45, 90, 34, 23, 56];
+let newAge = age.filter((val) => {
+  return val > 18;
+});
+console.log("They Can Vote");
---
+++ 10_Functions/Array Methods/Filter Method/02_Even.js
@@ -0,0 +1,5 @@
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let neweven = num.filter((val) => {
+  return val % 2 == 0;
+});
+console.log("They Are Even");
