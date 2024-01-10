8113a03b30073508a8162ca299f9b02db5902c05
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-01-10 13:42:51 +0530

    Delete 10_Function/For Each Loop In Array directory

--- 10_Function/For Each Loop In Array/01_concept.js
+++
@@ -1,5 +0,0 @@
-//Using Basic Function
-let arr=[1,2,3,4,5,6];
-arr.forEach(function concept(val){
-    console.log(val);
-})
--- 10_Function/For Each Loop In Array/02_Arrowcon.js
+++
@@ -1,4 +0,0 @@
-let arr=["pune","mumbai","delhi"];
-arr.forEach((val,idx,arr)=>{
-    console.log(val.toUpperCase(),idx,arr);
-})
