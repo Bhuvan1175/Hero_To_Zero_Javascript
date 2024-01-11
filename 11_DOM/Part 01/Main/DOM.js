2c9096c6b1d2d1d148e2171d0f8e5748761d2271
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-01-11 18:48:40 +0530

    Add files via upload

---
+++ 11_DOM/Part 01/Main/DOM.js
@@ -0,0 +1,8 @@
+// The method of accessing HTML code in JavaScript is through the Document Object Model (DOM).
+// The DOM Is used in Dynamic Changes And Manipulation.
+
+// HTMl Access
+console.dir(document.body.childNodes[3]);
+
+// Dynamic Chnages
+document.body.childNodes[1].style.color="Red";
