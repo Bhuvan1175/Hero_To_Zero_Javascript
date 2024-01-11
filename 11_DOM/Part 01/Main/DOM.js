1d7f65780a56164866f0cefbefa92100785f0275
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-01-11 18:56:25 +0530

    Update DOM.js

--- 11_DOM/Part 01/Main/DOM.js
+++ 11_DOM/Part 01/Main/DOM.js
@@ -1,5 +1,9 @@
 // The method of accessing HTML code in JavaScript is through the Document Object Model (DOM).
 // The DOM Is used in Dynamic Changes And Manipulation.
+// Why We link Script js just before ending body Tag?
+// --> If we write script tag befor body tag, then thats why the DOM Elments will not accessable.so thats why we write script tag on before ending body tag.
+// --> Because we want to make sure that all elements are loaded before our script runs.
+
 
 // HTMl Access
 console.dir(document.body.childNodes[3]);
