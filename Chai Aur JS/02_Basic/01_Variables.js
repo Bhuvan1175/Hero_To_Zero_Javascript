f7d9419385b64431c7f2944772427149a6b30e03
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-01-14 23:29:12 +0530

    Create 01_Variables.js

---
+++ Chai Aur JS/02_Basic/01_Variables.js
@@ -0,0 +1,18 @@
+var fullName = "Bhuvi Bagde";
+console.log(fullName);
+// Number Data Type
+var bhuviAge=21;
+
+// String Data Type
+var courseName = "Sigma Javascript";
+
+// Boolean Data Type
+var isfollow = true;
+
+//Undefined Data Type
+var paymentMode;
+console.log(paymentMode);
+
+paymentMode = "Credit Card";
+
+// We Cannot used Reserved Keyword As A Variable Name .
