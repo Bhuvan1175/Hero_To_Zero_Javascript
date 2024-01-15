c7c24855b30d31e8f3b242b028cc9819c4eda21e
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-01-15 19:22:38 +0530

    Add files via upload

---
+++ Chai Aur JS/02_Basic/09_Ternary.js
@@ -0,0 +1,12 @@
+// Show user signout button if he is authenticate
+// otherwise shows the login button
+
+var authenticated=false;
+// if(authenticated){
+//     console.log("Shows Signout Button");
+// }else{
+//     console.log("Show Login Button");
+// }
+
+
+authenticated ? console.log("Signout Button") : console.log("Login Button");
