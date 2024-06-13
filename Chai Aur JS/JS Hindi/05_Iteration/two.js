fc67d2a83270decc502f831abebcd90c9d1690c7
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-06-13 22:32:00 +0530

    Add files via upload

---
+++ Chai Aur JS/JS Hindi/05_Iteration/two.js
@@ -0,0 +1,23 @@
+// While Loop
+let i = 0;
+while (i <= 10) {
+  if (i % 2 == 0) {
+    console.log(`${i} is even Number`);
+  }
+  i++;
+}
+// Array
+
+let myArray = ["flash", "batman", "superman"];
+let arr = 0;
+while (arr < myArray.length) {
+  console.log(myArray[arr]);
+  arr++;
+}
+
+// Do While Loop
+let score = 1;
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
