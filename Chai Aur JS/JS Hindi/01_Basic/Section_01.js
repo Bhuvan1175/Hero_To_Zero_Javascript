7b477535d80cd0e6112b049ef503191ac781d27e
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-06-14 13:15:16 +0530

    Update and rename Section_01.txt to Section_01.js

---
+++ Chai Aur JS/JS Hindi/01_Basic/Section_01.js
@@ -0,0 +1,15 @@
+This Is a First Section in which we covered the topic like variable , Data Type , conversion , comparision and numbers and maths and Date Time ..
+
+In The Section 01 Basic 1 First File 
+
+1) The first file, titled "01_variable," covers the basics of variables in     
+   JavaScript, including the keywords const, let, and var. In this file, we 
+   practiced 
+   creating variables and printing them using console.log(). To avoid the hassle of 
+   printing each variable individually, we used console.table([]) to print all the 
+   variables in a single line. Additionally, we explored the scope of variables in 
+   JavaScript.
+     
+2) The second file , titled "02_dataType", covers the datatype used in javascript 
+   just like number , string, boolean , null , undefined , Bigint.Alos we checkk 
+   there type using "typeof()" .
