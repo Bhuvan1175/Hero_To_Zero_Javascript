acf35a009c683e1900271034bf6a30831a1444b4
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-07-18 21:59:35 +0530

    Create 01-console.js

---
+++ Delta Batch JS /PART- I/01-console.js
@@ -0,0 +1,9 @@
+// Console is used to print anything on terminal.
+// Console is a class in System namespace.
+console.log("hello world!");
+// This how we can print it our output message.
+// The Console.log is work based on REPL condition mean
+// Console firstly Read the Input 
+// Then is Check if it any expression if it is then it do evaluation if not then it also evalute.
+// After That It Will Print it an Output message.
+// And It will Contains in LOOP.
