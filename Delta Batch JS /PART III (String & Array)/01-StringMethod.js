3edecca120c108d3089ee8a7985ab8879d2006e8
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-07-22 14:34:59 +0530

    Create 01-StringMethod.js

---
+++ Delta Batch JS /PART III (String & Array)/01-StringMethod.js
@@ -0,0 +1,7 @@
+// Trim()
+// Trim method is used to trims whitespaces from both end of string & return a new one.
+// Syntax: string.trim()
+
+let msg="          Hello      "
+console.log(msg.trim()) // Hello
+console.log(msg); 
