90f09e8907c45773a7444411421cd91d509ffdd4
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-08-08 17:12:34 +0530

    Add files via upload

---
+++ 11_DOM/Part 02/six.js
@@ -0,0 +1,7 @@
+let div=document.createElement("div")
+div.style.backgroundColor="red";
+div.setAttribute("id","heros")
+let addText=document.createTextNode("danger Bhai")
+div.appendChild(addText)
+
+document.body.appendChild(div);
